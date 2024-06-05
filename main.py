948b7534c1dd7b27f90608bb367c43609f937300
Fading Raven <38802245+FadingRaven@users.noreply.github.com>
2024-06-06 03:10:19 +0800

    1.3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymysql import *
 
@@ -7,10 +7,34 @@ CORS(app)
 conn = None
 cursor = None
 try:
-    conn = connect(host='localhost', port=3306, user='root', password='123456', database='db_work')
+    conn = connect(host='localhost', port=3306, password='123456', user='root', database='db_work')
     cursor = conn.cursor()
+    sql = "SET GLOBAL event_scheduler = ON"
+    cursor.execute(sql)
+    conn.commit()
 except Exception as e:
     print(e)
+
+
+@app.route('/getip')
+def getip():
+    ip_address = request.remote_addr
+    try:
+        sql = f"SELECT * FROM blacklist WHERE clientip = \"{ip_address}\";"
+        cursor.execute(sql)
+        conn.commit()
+        result = cursor.fetchall()
+        print(result)
+        if len(result) == 0:
+            sql = f"insert into blacklist(clientip,level) values(\"{ip_address}\",'0')"
+            cursor.execute(sql)
+            conn.commit()
+            return ip_address
+        else:
+            return ip_address
+    except Exception as e:
+        print(e)
+        return "No"
 
 
 @app.route('/Logincheck/<usern>&&<passw>')
@@ -19,6 +43,7 @@ def logincheck(usern, passw):
     try:
         sql = f"SELECT * FROM user_password WHERE userid = \"{usern}\" AND password = \"{passw}\";"
         cursor.execute(sql)
+        conn.commit()
         result = cursor.fetchall()
         print(result)
         if len(result) > 0:
@@ -30,24 +55,87 @@ def logincheck(usern, passw):
         return "No"
 
 
-@app.route('/regcheck/<usern>&&<passw>&&<qtype>&&<answer>')
-def regcheck(usern, passw, qtype, answer):
-    print("regcheck:", usern, passw, qtype, answer)
+@app.route('/adminLogin/<usern>&&<passw>')
+def adminLogin(usern, passw):
+    print("adminLogin:", usern, passw)
     try:
-        sql = f"insert user_password values(\"{usern}\",\"{passw}\",\"{qtype}\",\"{answer}\")"
+        sql = f"SELECT * FROM admin WHERE adminid = \"{usern}\" AND password = \"{passw}\";"
         cursor.execute(sql)
-        return "Yes"
+        conn.commit()
+        result = cursor.fetchall()
+        print(result)
+        if len(result) > 0:
+            return "Yes"
+        else:
+            return "No"
     except Exception as e:
+        print(e)
         return "No"
 
 
-@app.route('/regcheckNoques/<usern>&&<passw>')
-def regcheckNoques(usern, passw):
-    print("regcheck:", usern, passw)
+@app.route('/regcheck/<usern>&&<passw>&&<qtype>&&<answer>&&<ip>')
+def regcheck(usern, passw, qtype, answer, ip):
+    print("regcheck:", usern, passw, qtype, answer, ip)
     try:
-        sql = f"insert user_password(userid,password) values(\"{usern}\",\"{passw}\")"
+        sql = f"select level from blacklist where clientip=\"{ip}\""
         cursor.execute(sql)
-        return "Yes"
+        conn.commit()
+        result = cursor.fetchall()
+        rr = None
+        for item in result:
+            rr = item[0]
+        if rr == '1':
+            sql = f"insert user_password values(\"{usern}\",\"{passw}\",\"{qtype}\",\"{answer}\")"
+            cursor.execute(sql)
+            conn.commit()
+            sql = f"update blacklist set level=level+1 where clientip=\"{ip}\""
+            cursor.execute(sql)
+            conn.commit()
+            return "warning"
+        if rr == '0':
+            sql = f"insert user_password values(\"{usern}\",\"{passw}\",\"{qtype}\",\"{answer}\")"
+            cursor.execute(sql)
+            conn.commit()
+            sql = f"update blacklist set level=level+1 where clientip=\"{ip}\""
+            cursor.execute(sql)
+            conn.commit()
+            return "Yes"
+        if rr == '2':
+            return "full"
+    except Exception as e:
+        print(e)
+        return "No"
+
+
+@app.route('/regcheckNoques/<usern>&&<passw>&&<ip>')
+def regcheckNoques(usern, passw, ip):
+    print("regcheck:", usern, passw, ip)
+    try:
+        sql = f"select level from blacklist where clientip=\"{ip}\""
+        cursor.execute(sql)
+        conn.commit()
+        result = cursor.fetchall()
+        rr = None
+        for item in result:
+            rr = item[0]
+        if rr == '1':
+            sql = f"insert user_password(userid,password) values(\"{usern}\",\"{passw}\")"
+            cursor.execute(sql)
+            conn.commit()
+            sql = f"update blacklist set level=level+1 where clientip=\"{ip}\""
+            cursor.execute(sql)
+            conn.commit()
+            return "warning"
+        if rr == '0':
+            sql = f"insert user_password(userid,password) values(\"{usern}\",\"{passw}\")"
+            cursor.execute(sql)
+            conn.commit()
+            sql = f"update blacklist set level=level+1 where clientip=\"{ip}\""
+            cursor.execute(sql)
+            conn.commit()
+            return "Yes"
+        if rr == '2':
+            return "full"
     except Exception as e:
         print(e)
         return "No"
@@ -58,6 +146,7 @@ def getAllBookInfo():
     try:
         sql = f"select * from bookinf"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
@@ -76,6 +165,7 @@ def addNewBook(bookid, bookname, booktype, bookauthor, total):
     try:
         sql = f"insert bookinf values(\"{bookid}\",\"{bookname}\",\"{booktype}\",\"{bookauthor}\",\"{total}\",\"0\")"
         cursor.execute(sql)
+        conn.commit()
         print(cursor.fetchall())
         return "success"
     except Exception as e:
@@ -89,6 +179,7 @@ def delBooks(bookid):
     try:
         sql = f"delete from bookinf where bookid = \"{bookid}\" "
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         return 'failed'
@@ -100,6 +191,7 @@ def alterBookInfo(bookid, bookname, booktype, bookauthor, total):
     try:
         sql = f"update bookinf set bookname = \"{bookname}\",booktype = \"{booktype}\",bookauthor = \"{bookauthor}\",total = \"{total}\" where bookid = \"{bookid}\" "
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         print(e)
@@ -109,28 +201,32 @@ def alterBookInfo(bookid, bookname, booktype, bookauthor, total):
 @app.route('/getAllBorrowInfo')
 def getAllBorrowInfo():
     try:
-        sql = "select no,userid,bookid,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow where returntime is null"
+        sql = "select no,userid,bookid,adminid,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow where returntime is null"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
             print(item)
             res.append(
-                {"no": item[0], "userid": item[1], "bookid": item[2], "borrowtime": item[3], "returntime": item[4]})
+                {"no": item[0], "userid": item[1], "bookid": item[2], "adminid": item[3], "borrowtime": item[4],
+                 "returntime": item[5]})
         print(res)
         return jsonify(res)
     except Exception as e:
         return 'failed'
 
 
-@app.route('/addNewBorrow/<userid>&&<bookid>&&<borrowtime>')
-def addNewBorrow(userid, bookid, borrowtime):
-    print("addNewBorrow:", userid, bookid, borrowtime)
+@app.route('/addNewBorrow/<userid>&&<bookid>&&<borrowtime>&&<adminid>')
+def addNewBorrow(userid, bookid, borrowtime, adminid):
+    print("addNewBorrow:", userid, bookid, borrowtime, adminid)
     try:
-        sql = f"insert into borrow (userid,bookid,borrowtime) values(\"{userid}\",\"{bookid}\",\"{borrowtime}\")"
+        sql = f"insert into borrow (userid,bookid,adminid,borrowtime) values(\"{userid}\",\"{bookid}\",\"{adminid}\",\"{borrowtime}\")"
         cursor.execute(sql)
+        conn.commit()
         sql = f"update bookinf set borrowed=borrowed+1 where bookid = \"{bookid}\""
         cursor.execute(sql)
+        conn.commit()
         print(cursor.fetchall())
         return "success"
     except Exception as e:
@@ -144,19 +240,22 @@ def delBorrow(no):
     try:
         sql = f"delete from borrow where no = \"{no}\" "
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         return 'failed'
 
 
-@app.route('/returnBorrow/<no>&&<returntime>')
-def returnBorrow(no, returntime):
-    print('returnBorrow:', no, returntime)
+@app.route('/returnBorrow/<no>&&<returntime>&&<adminid>')
+def returnBorrow(no, returntime, adminid):
+    print('returnBorrow:', no, returntime, adminid)
     try:
-        sql = f"update borrow set returntime= \"{returntime}\" where no = \"{no}\" "
+        sql = f"update borrow set returntime= \"{returntime}\",adminid=\"{adminid}\" where no = \"{no}\" "
         cursor.execute(sql)
+        conn.commit()
         sql = f"update bookinf set borrowed=borrowed-1 where bookid in (select bookid from borrow where no = \"{no}\")"
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         print(e)
@@ -167,14 +266,16 @@ def returnBorrow(no, returntime):
 def getAllBorrowInfoFiltered(userid, bookid, borrowtime):
     print('getAllBorrowInfoFiltered:', userid, bookid, borrowtime)
     try:
-        sql = f"select no,userid,bookid,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow where returntime is null and userid = \"{userid}\" and bookid = \"{bookid}\" and borrowtime = \"{borrowtime}\""
+        sql = f"select no,userid,bookid,adminid,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow where returntime is null and userid = \"{userid}\" and bookid = \"{bookid}\" and borrowtime = \"{borrowtime}\""
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
             print(item)
             res.append(
-                {"no": item[0], "userid": item[1], "bookid": item[2], "borrowtime": item[3], "returntime": item[4]})
+                {"no": item[0], "userid": item[1], "bookid": item[2], "adminid": item[3], "borrowtime": item[4],
+                 "returntime": item[5]})
         print(res)
         return jsonify(res)
     except Exception as e:
@@ -188,6 +289,7 @@ def getBorrowRankByDay():
     try:
         sql = "select  DATE_FORMAT(borrowtime, '%Y-%m-%d'),count(*) from borrow group by borrowtime"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
@@ -204,6 +306,7 @@ def getMostPopularBooks():
     try:
         sql = "select bookid,count(*) from borrow group by bookid order by count(*) desc"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
@@ -220,6 +323,7 @@ def getMostWelcomeUser():
     try:
         sql = "select userid,count(*) from borrow group by userid order by count(*) desc"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
@@ -236,8 +340,10 @@ def createNewBorrow(bookid, userid, date):
     try:
         sql = f"insert into borrow(userid,bookid,borrowtime) values(\"{userid}\",\"{bookid}\",\"{date}\")"
         cursor.execute(sql)
+        conn.commit()
         sql = f"update bookinf set borrowed = borrowed+1 where bookid =\"{bookid}\""
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         print(e)
@@ -247,15 +353,16 @@ def createNewBorrow(bookid, userid, date):
 @app.route('/getAllUserBorrow/<userid>')
 def getAllUserBorrow(userid):
     try:
-        sql = f"select no,bookname,bookauthor,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow,bookinf where userid=\"{userid}\" and borrow.bookid=bookinf.bookid order by borrowtime"
+        sql = f"select no,bookname,bookauthor,adminid,DATE_FORMAT(borrowtime, '%Y-%m-%d'),DATE_FORMAT(returntime, '%Y-%m-%d') from borrow,bookinf where userid=\"{userid}\" and borrow.bookid=bookinf.bookid order by borrowtime"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
             print(item)
             res.append(
-                {"no": item[0], "bookname": item[1], "bookauthor": item[2], "borrowtime": item[3],
-                 "returntime": item[4]})
+                {"no": item[0], "bookname": item[1], "bookauthor": item[2], "adminid": item[3], "borrowtime": item[4],
+                 "returntime": item[5]})
         print(res)
         return jsonify(res)
     except Exception as e:
@@ -269,6 +376,7 @@ def changePassword(oldP, newP, userid):
     try:
         sql = f"select password from user_password where userid=\"{userid}\""
         cursor.execute(sql)
+        conn.commit()
         rr = cursor.fetchall()
         res = ''
         for item in rr:
@@ -279,6 +387,7 @@ def changePassword(oldP, newP, userid):
         else:
             sql = f"update user_password set password= \"{newP}\" where userid=\"{userid}\""
             cursor.execute(sql)
+            conn.commit()
         return 'success'
     except Exception as e:
         print(e)
@@ -291,6 +400,7 @@ def delAccount(password, userid):
     try:
         sql = f"select password from user_password where userid=\"{userid}\""
         cursor.execute(sql)
+        conn.commit()
         rr = cursor.fetchall()
         res = ''
         for item in rr:
@@ -301,6 +411,7 @@ def delAccount(password, userid):
         else:
             sql = f"delete from user_password where userid=\"{userid}\""
             cursor.execute(sql)
+            conn.commit()
         return 'success'
     except Exception as e:
         print(e)
@@ -312,6 +423,7 @@ def getAllBookCanBorrow():
     try:
         sql = f"select bookid,bookname,booktype,bookauthor,(total-borrowed) as rem from bookinf where total-borrowed>0"
         cursor.execute(sql)
+        conn.commit()
         res = []
         rr = cursor.fetchall()
         for item in rr:
@@ -328,6 +440,7 @@ def changeSeQtoDefault(userid):
     try:
         sql = f"update user_password set question=null and answer=null where userid=\"{userid}\""
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         print(e)
@@ -340,6 +453,7 @@ def changeSeQ(userid, question, answer):
     try:
         sql = f"update user_password set question=\"{question}\" and answer=\"{answer}\" where userid=\"{userid}\""
         cursor.execute(sql)
+        conn.commit()
         return "success"
     except Exception as e:
         print(e)
@@ -352,6 +466,7 @@ def getSeQ(usern):
     try:
         sql = f"SELECT question FROM user_password WHERE userid = \"{usern}\""
         cursor.execute(sql)
+        conn.commit()
         rr = cursor.fetchall()
         res = ''
         for item in rr:
@@ -371,6 +486,7 @@ def confirmSeQ(usern, answer):
     try:
         sql = f"SELECT answer FROM user_password WHERE userid = \"{usern}\""
         cursor.execute(sql)
+        conn.commit()
         rr = cursor.fetchall()
         res = ''
         for item in rr:
@@ -379,6 +495,7 @@ def confirmSeQ(usern, answer):
         if answer == res:
             sql = f"SELECT password FROM user_password WHERE userid = \"{usern}\""
             cursor.execute(sql)
+            conn.commit()
             rr = cursor.fetchall()
             res = ''
             for item in rr:
